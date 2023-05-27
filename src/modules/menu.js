c087cef60421850922843d76d0d190c84d1b9113
Dima163163 <khlopovdima@gmail.com>
2023-05-27 15:34:03 +0400

    lesson20 усложненное

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,6 +3,9 @@ const menu = () => {
   const menu = document.querySelector("menu");
   const closeBtn = menu.querySelector(".close-btn");
   const menuItems = menu.querySelectorAll("ul>li>a");
+  const anchors = menu.querySelectorAll('ul>li>a[href*="#"]');
+  const serviceBlock = document.querySelector('a[href*="#"]');
+  console.log(serviceBlock);
 
   const handleMenu = () => {
     menu.classList.toggle("active-menu");
@@ -15,6 +18,25 @@ const menu = () => {
   menuItems.forEach((menuItem) =>
     menuItem.addEventListener("click", handleMenu)
   );
+
+  anchors.forEach((anchor) => {
+    anchor.addEventListener("click", function (event) {
+      event.preventDefault();
+      const blockID = anchor.getAttribute("href");
+      document.querySelector("" + blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  });
+  serviceBlock.addEventListener("click", function (event) {
+    event.preventDefault();
+    const serviceId = serviceBlock.getAttribute("href");
+    document.querySelector("" + serviceId).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
 };
 
 export default menu;
