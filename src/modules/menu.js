eaa0e78a6b046ec1a8ee888d5bffa62b976a7f52
Dima163163 <khlopovdima@gmail.com>
2023-05-28 16:14:00 +0400

    reqAnimation less20

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -10,15 +10,82 @@ const menu = () => {
     menu.classList.toggle("active-menu");
   };
 
-  const scrollIntoView = function (event) {
+  // const scrollIntoView = function (event) {
+  //   event.preventDefault();
+  //   const blockID = event.currentTarget.getAttribute("href");
+  //   const blockElem = document.querySelector("" + blockID);
+  //   console.log(blockElem);
+  //   blockElem.scrollIntoView({
+  //     behavior: "smooth",
+  //     block: "start",
+  //   });
+  // };
+
+  const smoothScroll = (target, duration) => {
+    console.log("target1", target);
+    console.log("duration", duration);
+    //текущая позиция в пикселях
+    let currentY = window.pageYOffset;
+    console.log("currentY", currentY);
+    //отслеживает позицию Y в пикселях
+    target = document.getElementById(target);
+    console.log("target2", target);
+
+    let targetYPos = target.offsetTop;
+    console.log("targetYPos", targetYPos);
+    //оставшееся расстояние до цели в пикселях
+    let distance = targetYPos - currentY;
+    console.log("distance", distance);
+    //отслеживание времни
+    let start = null;
+
+    const animation = (timestamp) => {
+      console.log("timestamp", timestamp);
+      console.log("start1", start);
+
+      if (!start) start = timestamp;
+
+      console.log("timestamp", timestamp);
+      console.log("start2", start);
+
+      let timeElapsed = timestamp - start;
+
+      let run = ease(timeElapsed, currentY, distance, duration);
+      console.log(run);
+      console.log("timeElapsed", timeElapsed);
+      console.log("currentY", currentY);
+      console.log("distance", distance);
+      console.log("duration", duration);
+
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) {
+        requestAnimationFrame(animation);
+      } else {
+        cancelAnimationFrame(animationID);
+      }
+    };
+
+    let animationID = requestAnimationFrame(animation);
+  };
+
+  let ease = (timeElapsed, currentY, distance, duration) => {
+    timeElapsed /= duration / 2;
+    if (timeElapsed < 1) {
+      return (
+        (distance / 2) * timeElapsed * timeElapsed * timeElapsed + currentY
+      );
+    }
+    timeElapsed -= 2;
+    return (
+      (distance / 2) * (timeElapsed * timeElapsed * timeElapsed + 2) + currentY
+    );
+  };
+
+  const scrollAnimation = function (event) {
     event.preventDefault();
-    const blockID = event.currentTarget.getAttribute("href");
-    const blockElem = document.querySelector("" + blockID);
-    console.log(blockElem);
-    blockElem.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
+    let scrollTarget = event.currentTarget.getAttribute("href").slice(1);
+    console.log("scrollTarget", scrollTarget);
+    smoothScroll(scrollTarget, 1000);
   };
 
   menuBtn.addEventListener("click", handleMenu);
@@ -29,10 +96,16 @@ const menu = () => {
     menuItem.addEventListener("click", handleMenu)
   );
 
-  anchors.forEach((anchor) => {
-    anchor.addEventListener("click", scrollIntoView);
-  });
-  serviceBlock.addEventListener("click", scrollIntoView);
+  menuItems.forEach((menuItem) =>
+    menuItem.addEventListener("click", scrollAnimation)
+  );
+
+  serviceBlock.addEventListener("click", scrollAnimation);
+
+  // anchors.forEach((anchor) => {
+  //   anchor.addEventListener("click", scrollIntoView);
+  // });
+  // serviceBlock.addEventListener("click", scrollIntoView);
 };
 
 export default menu;
