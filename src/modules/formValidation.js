846c9c1661bedda44e117e8bce85394da2260da7
Dima163163 <khlopovdima@gmail.com>
2023-05-29 17:56:59 +0400

    lesson21fix

--- src/modules/formValidation.js
+++ src/modules/formValidation.js
@@ -1,16 +1,29 @@
 const formValidation = () => {
   const formNameSelectors = document.querySelectorAll(".form-name");
+  const formNameTopSelector = document.querySelector(".top-form");
   const messageSelector = document.querySelector(".mess");
   const emailSelectors = document.querySelectorAll(".form-email");
   const phoneSelectors = document.querySelectorAll(".form-phone");
 
+  // const validtionMessage = function () {
+  //   this.value = this.value.replace(/^[a-zA-Z0-9]+$/, "");
+  // };
+
   const validtionMessage = function () {
-    this.value = this.value.replace(/^[a-zA-Z\s-]+$/, "");
+    const message = this.value;
+    const messagePattern = /^[а-яё]+(?:[ -]{1}[а-яё]*)?$/i;
+    const isValid = messagePattern.test(message);
+    if (!isValid) {
+      this.setCustomValidity("Неправильный формат имени");
+    } else {
+      this.setCustomValidity("");
+    }
+    this.reportValidity();
   };
 
   const validationEmail = function () {
     const email = this.value;
-    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const emailPattern = /[\-\.\w]+@([\w]+\.)+[\w]+/gi;
     const isValid = emailPattern.test(email);
     if (!isValid) {
       this.setCustomValidity("Неправильный формат email");
@@ -19,12 +32,11 @@ const formValidation = () => {
     }
 
     this.reportValidity();
-    console.log(this.checkValidity());
   };
 
   const validationPhone = function () {
     const phone = this.value;
-    const phonePattern = /^\+\d{11}$/;
+    const phonePattern = /\+\d{11}$/;
     const isValid = phonePattern.test(phone);
     if (!isValid) {
       this.setCustomValidity("Неправильный формат телефона");
@@ -40,6 +52,8 @@ const formValidation = () => {
   formNameSelectors.forEach((form) =>
     form.addEventListener("input", validtionMessage)
   );
+
+  formNameTopSelector.addEventListener("input", validtionMessage);
 
   emailSelectors.forEach((email) =>
     email.addEventListener("input", validationEmail)
