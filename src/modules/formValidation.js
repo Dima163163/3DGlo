3c4faa525b3ae2a38e147c1cf7d938b6df4b5a03
Dima163163 <khlopovdima@gmail.com>
2023-05-30 19:06:11 +0400

    lesson21hard в работе

--- src/modules/formValidation.js
+++ src/modules/formValidation.js
@@ -1,6 +1,7 @@
 const formValidation = () => {
   const formNameSelectors = document.querySelectorAll(".form-name");
   const formNameTopSelector = document.querySelector(".top-form");
+  const typeTextSelector = document.querySelectorAll("[type=text]");
   const messageSelector = document.querySelector(".mess");
   const emailSelectors = document.querySelectorAll(".form-email");
   const phoneSelectors = document.querySelectorAll(".form-phone");
@@ -9,60 +10,125 @@ const formValidation = () => {
   //   this.value = this.value.replace(/^[a-zA-Z0-9]+$/, "");
   // };
 
-  const validtionMessage = function () {
-    const message = this.value;
-    const messagePattern = /^[а-яё]+(?:[ -]{1}[а-яё]*)?$/i;
-    const isValid = messagePattern.test(message);
-    if (!isValid) {
-      this.setCustomValidity("Неправильный формат вода.Только кириллица.");
-    } else {
-      this.setCustomValidity("");
+  // const validtionMessage = function () {
+  //   let message = this.value;
+  //   const messagePattern = /^[а-яё]+(?:[ -]{1}[а-яё]*)?$/i;
+  //   const isValid = messagePattern.test(message);
+  //   if (!isValid) {
+  //     this.setCustomValidity("Неправильный формат вода.Только кириллица.");
+  //   } else {
+  //     this.setCustomValidity("");
+  //   }
+  //   this.reportValidity();
+  // };
+
+  const validtionName = function () {
+    let message = this.value.trim();
+
+    if (this.type === "text") {
+      message = capitalizedWords(message);
     }
-    this.reportValidity();
+    message = message.replace(/[-\s]{2,}/g, " ");
+    message = message.replace(/^-+|-$/g, "");
+    message = message.replace(/^[a-zA-Z]+(?:[-]{1}[a-zA-Z]*)?$/i, "");
+    this.value = message;
   };
 
+  const validtionMessage = function () {
+    let message = this.value.trim();
+    if (this.className === "mess") {
+      message = capitalizedWords(message);
+    }
+    message = message.replace(/[-\s]{2,}/g, " ");
+    message = message.replace(/^-+|-$/g, "");
+    message = message.replace(/^[a-zA-Z]+(?:[-]{1}[a-zA-Z]*)?$/i, "");
+    this.value = message;
+  };
+
+  function capitalizedWords(text) {
+    let words = text.split(" ");
+    const capitalizedWords = words.map(function (word) {
+      // Приводим первую букву к верхнему регистру, остальные буквы к нижнему
+      const capitalized =
+        word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+      return capitalized;
+    });
+    const result = capitalizedWords.join(" ");
+    return result;
+  }
+
   const validationEmail = function () {
-    const email = this.value;
+    const email = this.value.trim();
     const emailPattern = /[\-\.\w\_\.\!\~\*\']+@([\w]+\.)+[\w]+/gi;
     const isValid = emailPattern.test(email);
-    if (!isValid) {
-      this.setCustomValidity("Неправильный формат email");
-    } else {
-      this.setCustomValidity("");
-    }
-
-    this.reportValidity();
   };
 
   const validationPhone = function () {
-    const phone = this.value;
+    let phone = this.value.trim();
     console.log(phone);
-    const phonePattern = /\d{1}\(\d{3}\)\d{3}\-\d{2}\-\d{2}/g;
-    const isValid = phonePattern.test(phone);
+    const phonePattern =
+      /(\+7|8)[- _]*\(?[- _]*(\d{3}[- _]*\)?([- _]*\d){7}|\d\d[- _]*\d\d[- _]*\)?([- _]*\d){6})/g;
+    const result = phonePattern.replace(phonePattern);
     console.log(isValid);
-    if (!isValid) {
-      this.setCustomValidity("Формат телефона в виде: X(XXX)XXX-XX-XX");
-    } else {
-      this.setCustomValidity("");
-    }
-
-    this.reportValidity();
+    phone = phone.replace(/^-+|-$/g, "");
+    console.log(phone);
+    // phone = phone.replace(/[\d+]/g, (numb) => console.log(numb));
+    // const phonePattern = /\d{1}\(\d{3}\)\d{3}\-\d{2}\-\d{2}/g;
+    // const isValid = phonePattern.test(phone);
   };
 
-  messageSelector.addEventListener("input", validtionMessage);
+  // const validationEmail = function () {
+  //   const email = this.value.trim();
+  //   const emailPattern = /[\-\.\w\_\.\!\~\*\']+@([\w]+\.)+[\w]+/gi;
+  //   const isValid = emailPattern.test(email);
+  //   if (!isValid) {
+  //     this.setCustomValidity("Неправильный формат email");
+  //   } else {
+  //     this.setCustomValidity("");
+  //   }
 
-  formNameSelectors.forEach((form) =>
-    form.addEventListener("input", validtionMessage)
-  );
+  //   this.reportValidity();
+  // };
 
-  formNameTopSelector.addEventListener("input", validtionMessage);
+  // const validationPhone = function () {
+  //   const phone = this.value.trim();
+  //   console.log(phone);
+  //   const phonePattern = /\d{1}\(\d{3}\)\d{3}\-\d{2}\-\d{2}/g;
+  //   const isValid = phonePattern.test(phone);
+  //   console.log(isValid);
+  //   if (!isValid) {
+  //     this.setCustomValidity("Формат телефона в виде: X(XXX)XXX-XX-XX");
+  //   } else {
+  //     this.setCustomValidity("");
+  //   }
+  //   this.reportValidity();
+  // };
+
+  // messageSelector.addEventListener("input", validtionMessage);
+
+  // formNameSelectors.forEach((form) =>
+  //   form.addEventListener("input", validtionMessage)
+  // );
+
+  // formNameTopSelector.addEventListener("input", validtionMessage);
 
   emailSelectors.forEach((email) =>
     email.addEventListener("input", validationEmail)
   );
 
+  // phoneSelectors.forEach((phone) => {
+  //   phone.addEventListener("input", validationPhone);
+  // });
+
+  messageSelector.addEventListener("blur", validtionMessage);
+
+  formNameTopSelector.addEventListener("blur", validtionName);
+  formNameSelectors.forEach((form) =>
+    form.addEventListener("blur", validtionName)
+  );
+
   phoneSelectors.forEach((phone) => {
-    phone.addEventListener("input", validationPhone);
+    phone.addEventListener("blur", validationPhone);
   });
 };
 
